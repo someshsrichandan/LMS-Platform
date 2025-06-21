4d8de37da77e5adbb93f406812dd8fc7a6ce98ca
someshsrichandan <someshsrichandan@gmail.com>
2025-06-21 22:44:34 +0530

    arckjet added secur website is secure now

--- lib/env.ts
+++ lib/env.ts
@@ -9,6 +9,7 @@ export const env = createEnv({
     GITHUB_CLIENT_ID: z.string().min(1),
     GITHUB_CLIENT_SECRET: z.string().min(1),
     RESEND_API_KEY: z.string().min(1),
+    ARCJET_KEY: z.string().min(1),
 
   },
   // If you're using Next.js < 13.4.4, you'll need to specify the runtimeEnv manually
